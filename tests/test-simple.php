bf71ce5eab82aed05ce8bdd24d8910c0e4e870b6
paalsteek <github@paalsteek.de>
2015-04-28 10:43:24 +0200

    Fix expected test strings

--- tests/test-simple.php
+++ tests/test-simple.php
@@ -3,16 +3,18 @@
 class SimpleTests extends WP_UnitTestCase {
 	function test_beautify() {
 		$cmplist = array( // examples with and without grep param
-			array( 'tp-link-tl-wr740n-nd',  '',         'TP-Link TL-WR740N ND' ),
-			array( 'tp-link-tl-wr740n-nd',  'tp-link',  'TL-WR740N ND' ),
+			array( 'tp-link-tl-wr740n-nd',  '',         'TP-Link TL-WR740N/ND' ),
+			array( 'tp-link-tl-wr740n-nd',  'tp-link',  'TL-WR740N/ND' ),
 			array( 'tp-link-tl-wdr4300',    '',         'TP-Link TL-WDR4300' ),
 			array( 'tp-link-tl-wdr4300',    'tp-link',  'TL-WDR4300' ),
 			array( 'ubiquiti-unifi',        '',         'Ubiquiti Unifi' ),
 			array( 'ubiquiti-unifi',        'ubiquiti', 'Unifi' ),
 			array( 'ubiquiti-bullet-m',     '',         'Ubiquiti Bullet M2 / Nanostation Loco M2' ),
 			array( 'ubiquiti-bullet-m',     'ubiquiti', 'Bullet M2 / Nanostation Loco M2' ),
-			array( 'd-link-dir-615-rev-e1', '',         'D-Link DIR-615 rev e1' ),
-			array( 'd-link-dir-615-rev-e1', 'd-link',   'DIR-615 rev e1' ),
+			array( 'd-link-dir-615-rev-e1', '',         'D-Link DIR-615 REV E1' ),
+			array( 'd-link-dir-615-rev-e1', 'd-link',   'DIR-615 REV E1' ),
+			array( 'buffalo-wzr-hp-g450h',  '',         'Buffalo WZR-HP-G450H'),
+			array( 'buffalo-wzr-hp-g450h',  'buffalo',  'WZR-HP-G450H'),
 		);
 		foreach ( $cmplist as $item ) {
 			$expect = $item[2];
